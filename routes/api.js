4ac5c6450e6f073b26b6577c8bca6b93d0655df5
ConnorCleveland2002 <connorcleveland2002@gmail.com>
2021-08-24 18:17:03 -0400

    Final Push, total functionality

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,21 @@
 const router = require("express").Router();
 const Workout = require("../models/workoutModel.js");
-const ObjectId = require("mongodb");
-const path = require("path");
+const { ObjectId } = require("mongodb");
+// const path = require("path");
 const mongoose = require("mongoose");
 const castAggregation = require("mongoose-cast-aggregation");
 
 mongoose.plugin(castAggregation);
 
-router.get("/exercise", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/exercise.html"));
-});
-
-router.get("/stats", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/stats.html"));
+router.get("/exercise", (req, res) => {
+  Workout.find({})
+    .sort({ day: 1 })
+    .then(workoutDB => {
+      res.json(workoutDB)
+    })
+    .catch(err => {
+    res.status(400).json(err)
+  })
 });
 
 router.get("/api/workouts", (req, res) => {
@@ -43,16 +46,16 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
-router.get("/exercise", (req, res) => {
-  Workout.find({})
-    .sort({ day: 1 })
-    .then((workoutDB) => {
-      res.json(workoutDB);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
+// router.get("/exercise", (req, res) => {
+//   Workout.find({})
+//     .sort({ day: 1 })
+//     .then((workoutDB) => {
+//       res.json(workoutDB);
+//     })
+//     .catch((err) => {
+//       res.status(400).json(err);
+//     });
+// });
 
 router.post("/api/workouts", ({ body }, res) => {
   Workout.create(body)
@@ -90,11 +93,14 @@ router.put("/api/workouts/:id", (req, res) => {
 });
 
 router.get("/api/workouts/range", (req, res) => {
+  //Expects to recieve date in miliseconds, requries this extra step
   const endDate = new Date();
-  const startDate = new Date().setDate();
+  const startDate = new Date().setDate(new Date().getDate() - 6);
+  const beginDate = new Date(startDate);
+  const finalDate = new Date(endDate);
 
   Workout.aggregate([
-    { $match: { day: { $gte: startDate, $lte: endDate } } },
+    { $match: { day: { $gte: beginDate, $lte: finalDate } } },
   ])
     .project({
       'day': 1,
